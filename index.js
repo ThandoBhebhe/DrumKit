c4a39f7761a61d3e8382e089a2e28bcb29ec33af
thandobhebhe <bukhosi95@gmail.com>
2020-12-04 12:28:56 +0000

    added listeners to all buttons

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('this is me')
+for(let i = 0; i < document.querySelectorAll('button').length; i++){
+    document.querySelectorAll('button')[i].addEventListener('click', clicked);
+}
+
+function clicked (){
+    alert('I have been clicked');
+}
